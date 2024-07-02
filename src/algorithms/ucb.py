60f879e6c5e7528f07c8743d11e7482009a24741
aybuekeulusarslan <aybuke.ulusarslan@tum.de>
2024-07-02 20:06:03 +0200

    Initial commit for algorithms/ucb.py

--- src/algorithms/ucb.py
+++ src/algorithms/ucb.py
@@ -1,22 +1,41 @@
-# src/algorithms/ucb.py
 import numpy as np
+import sys, argparse
+sys.path.insert(0, 'C:/Users/aybuk/Git/causal-bandits/src/utils')
+from src.utils import MAB, SCM
 
-class UCB:
-    def __init__(self, n_arms):
-        self.n_arms = n_arms
-        self.counts = np.zeros(n_arms)
-        self.values = np.zeros(n_arms)
+def UCB1(bandit, rounds):
+    """
 
-    def select_arm(self):
-        total_counts = np.sum(self.counts)
-        if total_counts < self.n_arms:
-            return total_counts
-        ucb_values = self.values + np.sqrt((2 * np.log(total_counts)) / (self.counts + 1e-5))
-        return np.argmax(ucb_values)
+    :param bandit:
+    :param rounds:
+    :return: Total reward after all rounds, counts of pulls for each arm
+    """
+    n_arms = bandit.get_arms()
+    counts = np.zeros(n_arms) # Number of times each arm has been played
+    means = np.zeros(n_arms) # Mean reward for each arm
+    rewards = np.zeros(rounds) # Rewards obtained in each round
 
-    def update(self, chosen_arm, reward):
-        self.counts[chosen_arm] += 1
-        n = self.counts[chosen_arm]
-        value = self.values[chosen_arm]
-        new_value = ((n - 1) / n) * value + (1 / n) * reward
-        self.values[chosen_arm] = new_value
+    for t in range(rounds):
+        if t < n_arms:
+            # Play each arm once if time horizon allows for that
+            a_t = t
+        else:
+            # Compute upper confidence bounds
+            ucb_vals = means + np.sqrt((2 * np.log(t)) / counts)
+            a_t = np.argmax(ucb_vals)
+
+        # Pull the chosen arm
+        r_t = bandit.pull_arm(a_t)
+
+        # Update counts and mean values
+        counts[a_t] += 1
+        n = counts[a_t]
+        mu_t = means[a_t]
+        means[a_t] = ((n - 1) / n) * mu_t + (1 / n) * r_t
+
+        rewards[t] = r_t
+
+    print(f"Total reward after {rounds} rounds: {rewards.sum()}")
+    print(f"Number of times each arm was played: {counts}")
+
+    return rewards, counts
