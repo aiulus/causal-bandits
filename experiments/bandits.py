2036adff23021d4fc50f049b23a91a7f1542e4a7
aybuekeulusarslan <aybuke.ulusarslan@tum.de>
2024-07-14 13:53:07 +0200

    Applying most recent changes to experiments/bandits.py

--- experiments/bandits.py
+++ experiments/bandits.py
@@ -2,19 +2,23 @@ from pathlib import Path
 import sys
 import argparse
 import numpy as np
-from src.utils import MAB as MAB
-from src.utils import plots as plots
-from src.utils import io_mgmt as io_mgmt
-from src.algorithms import thompson as thompson
-from src.algorithms import ucb as UCB
-from src.algorithms import causal_thompson as causal_thompson
+
+import SCM, MAB, plots, io_mgmt
+import src.algorithms.thompson as thompson
+import src.algorithms.ucb as UCB
+import src.algorithms.causal_thompson as causal_thompson
+
+config = io_mgmt.configuration_loader()
+PATH_REWARDS = config['PATH_REWARDS']
 
 path_root = Path(__file__).parents[2]
 sys.path.append(str(path_root))
-print(sys.path)
 
-bandit = MAB.Bernoulli_MAB(10, 0.5)
-bandit.n_arms
+
+# print(sys.path)
+
+# bandit = MAB.Bernoulli_MAB(10, 0.5)
+# bandit.n_arms
 
 
 def run_thompson(bandit, bandit_type, n_iterations):
@@ -53,6 +57,8 @@ def main():
                         help="Epsilon (noise standard deviation) for Linear bandit.")
     parser.add_argument('--algorithm', type=str, choices=['thompson', 'ucb'],
                         help="Please specify the type of algorithm to use. Currently supported: ['thompson', 'ucb']")
+    parser.add_argument('--reward_type', type=str, required=True, choices=['cumulative', 'simple'],
+                        help="Please specify the reward type. Currently supported: ['cumulative', 'simple']")
     parser.add_argument('--save', action='store_true')
 
     args = parser.parse_args()
@@ -62,21 +68,29 @@ def main():
     bandit_kwargs = {
         'n_arms': args.n_arms,
         'p_true': args.p,
-        'means': args.mu,
-        'variances': args.sigma_sqr,
-        'context_dim': args.context_dim,
-        'theta': np.array(args.theta),
-        'epsilon': args.epsilon,
+        'means': args.mu if args.mu else None,
+        'variances': args.sigma_sqr if args.sigma_sqr else None,
+        'context_dim': args.context_dim if args.context_dim else None,
+        'theta': np.array(args.theta) if args.theta else None,
+        'epsilon': args.epsilon if args.epsilon else None,
         'scm': SCM.load_scm(args.scm_path) if args.scm_path else None,
         'reward_variable': 'reward_variable'  # Replace with actual reward variable name if necessary
     }
-
-    bandit = MAB.create_bandit(args.bandit_type, *args)
+    bandit = MAB.create_bandit(args.bandit_type, **{k: v for k, v in bandit_kwargs.items() if v is not None})
 
     if args.algorithm == 'thompson':
-        rewards = run_thompson(bandit, args.bandit_type, args.T)
+        rewards_simple, rewards_cumulative = run_thompson(bandit, args.bandit_type, args.T)
     elif args.algorithm == 'ucb':
-        rewards = UCB.UCB1(bandit, args.T)
+        rewards_simple, rewards_cumulative = UCB.UCB1(bandit, args.T)
 
-    io_mgmt.save_rewards_to_csv(rewards)
+    rewards_filename = PATH_REWARDS + f"{args.algorithm}_{args.n_arms}-armed_{args.bandit_type}_bandit_T{args.T}_{args.reward_type}-rewards"
+    if args.reward_type == 'simple':
+        rewards = rewards_simple
+    elif args.reward_type == 'cumulative':
+        rewards = rewards_cumulative
+    io_mgmt.save_rewards_to_csv(rewards, rewards_filename)
     plots.plot_rewards(rewards, args.algorithm, args.bandit_type, args.save)
+
+
+if __name__ == '__main__':
+    main()
